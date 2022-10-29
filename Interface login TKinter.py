61a33bfabc0b6b5bca6e0c3524fab0709816d922
Izabel Cristina <115845859+Izabel-Cristina@users.noreply.github.com>
2022-10-29 00:34:01 -0300

    Add files via upload
    Adicionado as mensagens de aviso

--- Interface login TKinter.py
+++ Interface login TKinter.py
@@ -4,6 +4,14 @@ janela = tk.Tk()
 janela.rowconfigure(0, weight = 1)
 janela.columnconfigure([0,1],weight = 1)
 janela.title("Faça o seu login aqui:")
+def login():
+    usuario1 = 'Usuario'
+    senha1 = '1234'
+    if usuario.get()==usuario1 and senha.get()==senha1:
+        messagebox.showinfo(title='login', message = 'Login efetuado com sucesso')
+    else:
+       messagebox.showinfo(title='login', message = 'Login ou senha invalida')
+
 mensagem1 = tk.Label(text = "Entre com o seu usuário e senha:", fg = "white", bg = "black", width =40, height = 1)
 mensagem1.grid(row = 0, column = 0, columnspan = 2, sticky = "NSWE")
 mensagem2 = tk.Label(text = "Login", height = 2)
@@ -17,8 +25,9 @@ senha = tk.Entry(janela, show= '*')
 senha.grid( row = 3, column = 1)
 
 
-botao = tk.Button(text = "Acesse", height = 1, width = 5)
+botao = tk.Button(text = "Acesse",command = login, height = 1, width = 5)
 botao.grid(row = 5, column = 1)
+
 
 janela.mainloop()
 
